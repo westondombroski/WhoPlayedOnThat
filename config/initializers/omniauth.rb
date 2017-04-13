9ac2f2efd3f885b270b1c4964be7b93af30c0641
westondombroski <westondombroski@gmail.com>
2017-04-13 09:30:20 -0700

    omniauth.rb variable names

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 keys = Rails.application.secrets
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['spotify_client_id'], ENV['spotify_client_secret'], scope: 'playlist-read-private user-read-private user-read-email'
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'playlist-read-private user-read-private user-read-email'
 end
