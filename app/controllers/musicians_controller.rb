2ca668f716c63e446d21e8be6cd3098e30be9383
westondombroski <westondombroski@gmail.com>
2017-04-12 21:25:00 -0700

    styling stuff, still working on the playlist stuff

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,8 +1,6 @@
 class MusiciansController < ApplicationController
   def show
-    breadcrumbs.add 'Tracks', tracks_path_url
-    breadcrumbs.add 'Track', track_show_path_url
-    breadcrumbs.add 'Musician', musician_path_url
+    breadcrumbs.add 'track', track_show_path_url
     participant_id = params[:id]
     response = Musician.get_track_by_participant_id(participant_id)
     @musicians = response['Results']
