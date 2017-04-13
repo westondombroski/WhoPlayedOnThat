2d4668fc9e4bbc505e20fe21efdeff8d875bdc59
westondombroski <westondombroski@gmail.com>
2017-04-13 09:28:32 -0700

    omniauth.rb weirdness

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 keys = Rails.application.secrets
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, keys.['spotify_client_id'], keys.['spotify_client_secret'], scope: 'playlist-read-private user-read-private user-read-email'
+  provider :spotify, ENV['spotify_client_id'], ENV['spotify_client_secret'], scope: 'playlist-read-private user-read-private user-read-email'
 end
